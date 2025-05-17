87e1c5628cd90db26430bf63263188c8925220a3
Sofonias Girmay <sofonias.dev@gmail.com>
2025-05-17 12:04:49 +0300

    update prisma client config

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient {
