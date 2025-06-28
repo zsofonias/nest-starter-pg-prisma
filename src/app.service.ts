b3ce4d603e5ed20daeaa456dcbf9eba3c2c65407
Sofonias Girmay <sofonias.dev@gmail.com>
2025-06-28 13:05:49 +0300

    fix: transfer-response-interceptor

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { DatabaseService } from './database/database.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly databaseService: DatabaseService) {}
-  getHello(): string {
-    this.databaseService.user.findMany();
-    return 'Hello World!';
-  }
+  constructor() {}
 }
